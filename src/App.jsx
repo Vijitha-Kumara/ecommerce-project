22ed233cd9affcd46f3cc0e6c816db32d0fe4fa9
Vijitha Kumara Ranamuka <vijitha.kumara.wu@gmail.com>
2026-05-18 12:17:00 +0530

    feat: create order, tracking, and checkout components with CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import CheckOutPage from "./pages/CheckOutPage"
+import Tracking from "./pages/Tracking"
+import Orders from "./pages/Orders"
+
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +12,8 @@ function App() {
       <Routes>
          <Route index  element ={ <HomePage/>}/>
          <Route path="/checkout" element ={<CheckOutPage/>}/>
+         <Route path="/Tracking" element ={ <Tracking/>}/>
+         <Route path="/Orders" element ={ <Orders/>}/>
       </Routes>
   
     </>
